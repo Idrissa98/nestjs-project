bfb9112a9cfa5991a647e955c09ea0ec4e8c801d
Eddy <id.hemedy98@gmail.com>
2023-05-22 13:28:00 +0200

    Adding my first module and it's controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { QuizController } from './modules/quiz/quiz.controller';
+import { QuizModule } from './modules/quiz/quiz.module';
 
 @Module({
-  imports: [],
+  imports: [QuizModule],
   controllers: [AppController],
   providers: [AppService],
 })
