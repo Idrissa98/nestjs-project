c2c2944ff46a59760cb71d4d832dd30d1aa131da
Eddy <id.hemedy98@gmail.com>
2023-05-25 16:19:10 +0200

    Restarting All

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -9,9 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-
-  @Get('/something')
-  setSomething(): string {
-    return this.appService.getSomething();
-  }
 }
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { QuizModule } from './modules/quiz/quiz.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typOrmConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [
-    QuizModule,
-    TypeOrmModule.forRoot(typOrmConfig)],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
- 
