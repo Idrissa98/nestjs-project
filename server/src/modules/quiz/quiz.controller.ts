1fee7d1bd4d42c8d60a93cb3e03e478857be8670
Eddy <id.hemedy98@gmail.com>
2023-05-27 17:51:18 +0200

    Saving data to database using repository file

--- server/src/modules/quiz/quiz.controller.ts
+++ server/src/modules/quiz/quiz.controller.ts
@@ -14,7 +14,7 @@ export class QuizController {
     @Post('/create')
     @HttpCode(200)
     @UsePipes(ValidationPipe)
-    createQuiz(@Body() quizData: CreateQuizDto) {
-        return { data: quizData };
+    async createQuiz(@Body() quizData: CreateQuizDto) {
+        return await this.quizService.createNewQuiz(quizData);
     }
 }
--- server/src/modules/quiz/quiz.service.ts
+++ server/src/modules/quiz/quiz.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { QuizRepository } from "./quiz.repository";
+import { CreateQuizDto } from "./dto/createQuiz.dto";
 
 @Injectable()
 export class QuizService {
+    constructor(@InjectRepository(QuizRepository) private quizRepository: QuizRepository) {}
+
     getAllQuiz() {
         return [1, 2, 3, 4, 'From the service']
+    }
+    async createNewQuiz(quiz: CreateQuizDto) {
+        return await this.quizRepository.save(quiz)
     }
 }
